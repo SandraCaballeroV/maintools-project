3ee69a51039c54e226423239fe4734bd829673f8
Alejandro Rueda <alejandrorueda96@hotmail.com>
2021-10-10 19:19:10 -0500

    agragar una tabla simple

--- gestion-ventas/src/Components/ventas/RegistrarVenta.jsx
+++ gestion-ventas/src/Components/ventas/RegistrarVenta.jsx
@@ -5,7 +5,28 @@ const RegistrarVenta = () =>{
         <div> 
             <Cabecera /> 
              Venta registrado
+             <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Direccion</th>
+            <th>Numero</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Usuario</td>
+            <td>Direccion</td>
+            <td>########</td>
+            <td>Correoelectronico</td>
+          </tr>
+       </tbody>
+      </table>
+    </div>
         </div>
+        
     )
         
     
